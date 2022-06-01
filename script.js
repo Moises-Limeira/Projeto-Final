7dc03191b8daf00202f74616b7efb0086a351a9d
Moises-Limeira <93412231+Moises-Limeira@users.noreply.github.com>
2022-05-31 21:27:41 -0300

    Finalizado

--- script.js
+++ script.js
@@ -1,69 +1,17 @@
-const imgProduto = document.querySelector(".foto1");
-const img= document.querySelector(".foto2");
-const png= document.querySelector(".foto3");
+const container = document.querySelectorAll(".container");
 async function dados() {
 let dados = await fetch("http://www.mocky.io/v2/5b15c4923100004a006f3c07");
 let descriçao = await  dados.json();
-//fetch('http://www.mocky.io/v2/5b15c4923100004a006f3c07')
-// .then(descriçao => dados.json())
-// .then(json => console.log(imgProduto.innerHTML = `<img src="${json.items[0].product.imageObjects[0].large}" alt="" />`));
-
-console.log(imgProduto)
-
-imgProduto.innerHTML = (`<img class="fotoProduto"  src="${descriçao.items[0].product.imageObjects[0].large}" alt="" />`);
-img.innerHTML = (`<img class="fotoProduto"  src="${descriçao.items[1].product.imageObjects[0].large}" alt="" />`);
-png.innerHTML = (`<img class="fotoProduto"  src="${descriçao.items[2].product.imageObjects[0].large}" alt="" />`);
-}
-
-dados()
-
-
-const valor = document.querySelector(".valor");
-const valor2 = document.querySelector(".valor2");
-const valor3 = document.querySelector(".valor3");
-async function aa() {
-let aa = await fetch("http://www.mocky.io/v2/5b15c4923100004a006f3c07");
-let tt = await  aa.json();
-
-console.log(valor)
-
-valor.innerHTML = (`<span>${tt.items[0].product.priceSpecification.price}`);
-valor2.innerHTML = (`<span>${tt.items[1].product.priceSpecification.price}`);
-valor3.innerHTML = (`<span>${tt.items[2].product.priceSpecification.price}`);
-
-}
-
-aa()
-
-
-
-
-
-
-const text = document.querySelector(".texto");
-const text2 = document.querySelector(".texto2");
-const text3 = document.querySelector(".texto3");
-async function ll() {
-let ll = await fetch("http://www.mocky.io/v2/5b15c4923100004a006f3c07");
-let jj = await  ll.json();
-
-console.log(text)
-
-text.innerHTML = (`<p class="texto">${jj.items[0].product.name}`);
-text2.innerHTML = (`<p class="texto">${jj.items[1].product.name}`);
-text3.innerHTML = (`<p class="texto">${jj.items[2].product.name}`);
-
-}
-
-ll()
-
-
-
-
-
-
-
-
+    descriçao.items.forEach(function(produto, index){
+        let fotoProduto = container[index].querySelector(".fotoProduto");
+        let nomeProduto = container[index].querySelector(".texto");
+        let preco = container[index].querySelector(".valor");
+        fotoProduto.setAttribute('src', produto.product.imageObjects[0].large);
+        nomeProduto.innerText = produto.product.name;
+        preco.innerText = produto.product.priceSpecification.price;
+    });
+}    
+dados();
 
 let sacola = document.querySelector(".sacola");
 let arrayProdutos = document.querySelectorAll("button");
@@ -73,7 +21,6 @@ sacola.innerHTML = produtosSacola.length;
 arrayProdutos.forEach(function(produto) {
   produto.addEventListener("click", function() {
    let produtoSerAdicionado =  this.closest(".container").getAttribute("data-id");
-//    console.log(this.closest(".container"));
     if (!produtosSacola.length) {
       produtosSacola.push({"idProduto": produtoSerAdicionado});
     } 
@@ -88,26 +35,3 @@ arrayProdutos.forEach(function(produto) {
   });
   
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
